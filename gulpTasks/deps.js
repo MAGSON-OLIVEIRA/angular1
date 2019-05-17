762a4967eb340f03d5faf6ee87888c1ec8dceade
magson_oliveira <magsond@gmail.com>
2019-05-16 23:21:59 -0300

    Signed-off-by: magson_oliveira <magsond@gmail.com>

---
+++ gulpTasks/deps.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const uglifycss = requere('gulp-uglifycss')
+
+gulp.task('deps', ['deps.css','deps.js','deps.fonts'])
+
+gulp.task('deps.css', () => {
+
+})
+
+gulp.task('deps.js', () => {
+
+})
+
+gulp.task('deps.fonts', () => {
+
+})
