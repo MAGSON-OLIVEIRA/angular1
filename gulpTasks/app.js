0791766f54f53b4285f5f9a7fab6f1a8ac906472
MAGSON-OLIVEIRA <magsond@gmail.com>
2019-06-02 14:42:24 -0300

    atualização

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,17 +1,36 @@
 const gulp = require('gulp')
-gulp.task('app', ['app.html','app.css','app.js','app.assets'])
-
-gulp.task('app.html', () => {
-
-})
-
-gulp.task('app.css', () => {
-
-})
-
-gulp.task('app.js', () => {
-
-})
-gulp.task('app.assets', () => {
-
-})
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
+gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
+gulp.task('app.html', () =>
+     gulp.src('app/**/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('public'))
+)
+gulp.task('app.css', () =>
+     gulp.src('app/**/*.css')
+        .pipe(uglifycss({
+            /* 'ublyComments': true */
+            "uglyComments": true
+        }))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('public/assets/css'))
+)
+gulp.task('app.js', () =>
+     gulp.src('app/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/assets/js'))
+)
+gulp.task('app.assets', () =>
+     gulp.src('assets/**/*.*')
+        .pipe(gulp.dest('public/assets'))
+)
