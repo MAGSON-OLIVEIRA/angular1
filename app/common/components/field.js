152f1560712e51b963a7b774551a8eb3ffd9ab23
MAGSON-OLIVEIRA <magsond@gmail.com>
2019-06-16 22:21:35 -0300

    Fazendo formulario de inclusão

---
+++ app/common/components/field.js
@@ -0,0 +1,31 @@
+(function(){
+    angular.module('primeiroApp').component('field',{
+        bindings:{
+            id: '@',
+            label: '@',
+            grid: '@',
+            placeholder: '@',
+            type: '@',
+        },
+        controller: [
+            'gridSystem',
+           // function(gridSystem){
+            //    this.gridClasses = gridSystem.toCssClasses(this.grid)
+            //}
+            function(gridSystem){
+                this.$onInit = function(){
+                    this.gridClasses = gridSystem.toCssClasses(this.grid)
+                }
+            }
+        ],
+        template: `
+        <div class=" {{ $ctrl.gridClasses }} ">
+            <div class="form-grup">
+                <label for=" {{ $ctrl.id }} "  > {{ $ctrl.label }} </label>
+                <input id=" {{ $ctrl.id }} " class="form-control"  placeholder=" {{ $ctrl.placeholder }} "
+                    type=" {{ $ctrl.type }} " />
+            </div>
+        </div>
+        `
+    })
+})()
