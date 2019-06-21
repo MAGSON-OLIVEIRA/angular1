ba65b9c40ae0ac06853c444524c78a717d551ddc
MAGSON-OLIVEIRA <magsond@gmail.com>
2019-06-20 23:52:58 -0300

    Fazendo formulario de inclusão

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -2,21 +2,50 @@
     angular.module('primeiroApp').controller('BillingCycleController',[
         '$http', 
         'msgs',
+        'tabs',
         BillingCycleController
     ])
-    function BillingCycleController($http, msgs){
+    function BillingCycleController($http, msgs, tabs){
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
  
 
+        vm.refresh = function(){
+            $http.get(url).then(function(response){
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+                tabs.show(vm, {tabList:true, tabCreate:true})
+            })
+        }
+
         vm.create = function() {
             $http.post(url, vm.billingCycle).then(function(response){
-                vm.billingCycle = {}
+                vm.refresh()
                 msgs.addSuccess('Operacao realizada com sucesso !!')
             }).catch(function(response){
                 msgs.addError(response.data.errors)
             })
         }
 
+        vm.showTabUpdate = function(billingCycle){
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabUpdate: true})
+        }
+
+        vm.showTabDelete = function(billingCycle){
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabDelete: true})
+        }
+
+        vm.delete = function(){
+            const deleteUrl = `${url}/${vm.billingCycle._id}`
+            $http.delete(deleteUrl, vm.billingCycle).then(function(response){
+                vm.refresh()
+                msgs.addSuccess('Operaçao realizada com sucesso!')
+            }).catch(function(response){
+                msgs.addError(response.data.errors)
+            })
+        }
+        vm.refresh()
     }
 })()
